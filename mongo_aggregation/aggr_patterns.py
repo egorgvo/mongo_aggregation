4fdf8a49d9ffa58a87546903b75dea3c65a68f02
egvo <work.egvo@ya.ru>
2020-09-08 16:40:41 +0400

    String args dollar prefixed now in merge_objects.

--- mongo_aggregation/aggr_patterns.py
+++ mongo_aggregation/aggr_patterns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from .patterns import _convert_names_with_underlines_to_dots
+import six
+
+from .patterns import _convert_names_with_underlines_to_dots, dollar_prefix
 
 
 def merge_objects(*args, **kwargs):
@@ -10,6 +12,7 @@ def merge_objects(*args, **kwargs):
     From MongoDB version 3.6."""
     # Put everything into args
     args = list(args)
+    args = [dollar_prefix(arg) if isinstance(arg, six.string_types) else arg for arg in args]
     if kwargs:
         kwargs = _convert_names_with_underlines_to_dots(kwargs, convert_operators=True)
         args.append(kwargs)
